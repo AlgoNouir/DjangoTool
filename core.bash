8fb195301557007678d3bdf32efe98deb18f69f8
algonouir <algo.mahdi.nouri@gmail.com>
2023-06-25 14:30:28 +0330

    set .env file project install checker

--- core.bash
+++ core.bash
@@ -5,17 +5,45 @@
 
 # create project
 
+
+function getEnv {
+    cat .env  2> /dev/null | grep "$1" | awk -F '[=]' '{ print $2 }'
+}
+
 function create_project () {
     read -p "insert your project name: " name
     django-admin startproject "${name^^}" .
     mkdir Apps
+    echo ALGO_DJANGO_TOOL="installed" >> .env
+}
+
+function create_app () {
+    cd Apps || mkdir Apps && cd Apps || exit
 }
 
 
 
+
+
+# check project installed
+if [ "$(getEnv ALGO_DJANGO_TOOL)" != 'installed' ] && [ "$1" != "startproject" ]; then
+    read -p "env not set would you want to install project? [Y/n ] " ans
+    if [ "$ans" != "n" ];then
+        create_project
+    fi
+
+fi
+
 # core
 case $1 in
     startproject)
-        create_project
+        if [  "$(getEnv ALGO_DJANGO_TOOL)" != 'installed'  ]; then
+            create_project
+        else
+            echo "you allredy install the project"
+        fi
+    ;;
+    createapp)
+        create_app
     ;;
 esac
