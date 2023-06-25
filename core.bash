6f0b6b3a6accc9cdcc0d686cb34f46b17767c181
algonouir <algo.mahdi.nouri@gmail.com>
2023-06-25 15:15:58 +0330

    format and set settings in django
    multiple program (add lang case)

--- core.bash
+++ core.bash
@@ -6,44 +6,231 @@
 # create project
 
 
+djangoSetting=$(cat <<- EOM
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                      IMPORTS                                      |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+from pathlib import Path
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                      SECURITY                                     |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-d=v=t!s=yyho4i(r597@*28rs0m^d$dcsz(kjx&*@*5)s)=vv3'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                        CORS                                       |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+ALLOWED_HOSTS = []
+
+
+# cors
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                   INSTALLED APPS                                  |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+DEPENDENCIES=[
+    'rest_framework',
+    "corsheaders",
+]
+
+MAIN_APPS = [
+
+]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    *DEPENDENCIES
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                     DEFINITION                                    |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+ROOT_URLCONF = 'SERVER.urls'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'SERVER.wsgi.application'
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                                  DATABASE & FILES                                 |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / '../data/db.sqlite3',
+    }
+}
+
+STATIC_URL = 'static/'
+
+
+# ----------------------------------------------------------------------------------+
+#                                                                                   |
+#                               INTERNATIONALIZATION                                |
+#                                                                                   |
+# ----------------------------------------------------------------------------------+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+EOM
+)
+
+
 function getEnv {
     cat .env  2> /dev/null | grep "$1" | awk -F '[=]' '{ print $2 }'
 }
 
-function create_project () {
+function django_create_project () {
     read -p "insert your project name: " name
+    if [ "$name" = "" ];then
+        name="server"
+    fi
+    pipenv shell
+    python3 -m pip install django django-cors-headers djangorestframework
     django-admin startproject "${name^^}" .
     mkdir Apps
+    echo "$djangoSetting"  > "./${name^^}/settings.py" 
     echo ALGO_DJANGO_TOOL="installed" >> .env
 }
 
-function create_app () {
+function django_create_app () {
     cd Apps || mkdir Apps && cd Apps || exit
 }
 
 
+function test () {
+    echo "$1" # arguments are accessible through $1, $2,...
+}
 
 
 
-# check project installed
-if [ "$(getEnv ALGO_DJANGO_TOOL)" != 'installed' ] && [ "$1" != "startproject" ]; then
-    read -p "env not set would you want to install project? [Y/n ] " ans
-    if [ "$ans" != "n" ];then
-        create_project
-    fi
 
-fi
+
+
 
 # core
-case $1 in
-    startproject)
-        if [  "$(getEnv ALGO_DJANGO_TOOL)" != 'installed'  ]; then
-            create_project
-        else
-            echo "you allredy install the project"
+case $1 in 
+
+    django)
+
+        # check project installed
+        if [ "$(getEnv ALGO_DJANGO_TOOL)" != 'installed' ] && [ "$1" != "startproject" ]; then
+            read -p "env not set would you want to install project? [Y/n ] " ans
+            if [ "$ans" != "n" ];then
+                django_create_project
+            fi
+
         fi
+
+        # django core
+        case $2 in
+            startproject)
+                if [  "$(getEnv ALGO_DJANGO_TOOL)" != 'installed'  ]; then
+                    django_create_project
+                else
+                    echo "you allredy install the project"
+                fi
+            ;;
+            createapp)
+                django_create_app
+            ;;
+        esac
     ;;
-    createapp)
-        create_app
-    ;;
+
 esac
